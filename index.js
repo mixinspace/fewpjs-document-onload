a1dc078ee02a1c76a648eec73c06b554b42513a8
mixinspace <islammadkaur@gmail.com>
2020-12-10 13:33:05 -0600

    commit tree 7f94652d63038ee18853ca25b1ccd6948dc27fa7

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const text = document.getElementById("text")
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    text.innerHTML = "This is really cool!"
+})
